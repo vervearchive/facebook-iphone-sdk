1b29bff72ec6c05dcef8e2275968e50d3d232cc9
Stephane Peter <megastep@megastep.org>
2010-01-08 16:58:20 +0800

    More reliable MD5 computation.

--- src/FBRequest.m
+++ src/FBRequest.m
@@ -70,11 +70,11 @@ static const NSTimeInterval kTimeoutInterval = 180.0;
   unsigned char result[CC_MD5_DIGEST_LENGTH];
   CC_MD5(str, strlen(str), result);
 
-  return [NSString stringWithFormat:
-    @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-    result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
-    result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]
-  ];
+  NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
+  for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+	[ret appendFormat:@"%02x",result[i]];
+  }
+  return ret;
 }
 
 - (BOOL)isSpecialMethod {
@@ -130,7 +130,7 @@ static const NSTimeInterval kTimeoutInterval = 180.0;
   } else if (_session.apiSecret) {
     [joined appendString:_session.apiSecret];
   }
-  
+
   return [self md5HexDigest:joined];
 }
 
